92fdf499629e8f704026cfcc7683197ed9f1d358
abdul <abdul.tanveer@gmail.com>
2021-06-23 10:50:46 +0530

    testing json

--- SpUnitTest/src/main/java/dnc/sp/unit/tests/HelloResource.java
+++ SpUnitTest/src/main/java/dnc/sp/unit/tests/HelloResource.java
@@ -1,14 +1,54 @@
 package dnc.sp.unit.tests;
 
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
-	
+
 	@GetMapping("/hello")
 	public String sayHello(){
 		return "hello abdul";
 	}
+
+	@GetMapping(value ="/student", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Student getStudent() {
+		return new Student("abdul", 123);
+	}
+
+	private class Student {
+
+		String name;
+		int age;
+		public Student(String name, int age) {
+			super();
+			this.name = name;
+			this.age = age;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return super.toString();
+		}
+
+
+
+	}
+
 
 }
--- SpUnitTest/src/test/java/dnc/sp/unit/tests/HellResourceTest.java
+++ SpUnitTest/src/test/java/dnc/sp/unit/tests/HellResourceTest.java
@@ -1,7 +1,9 @@
 package dnc.sp.unit.tests;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import junit.framework.TestCase;
 
 @SpringBootTest
 public class HellResourceTest {
@@ -24,13 +24,24 @@ public class HellResourceTest {
 
 	
 
-	@Test
-	public void testSayHello() throws Exception {
-		mockMvc = MockMvcBuilders.standaloneSetup(helloResource).build();
-
-		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
-		.andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(MockMvcResultMatchers.content().string("hello abdul"));
-	}
+	
+	/*
+	 * @Test public void testSayHello() throws Exception { mockMvc =
+	 * MockMvcBuilders.standaloneSetup(helloResource).build();
+	 * 
+	 * mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+	 * .andExpect(MockMvcResultMatchers.status().isOk())
+	 * .andExpect(MockMvcResultMatchers.content().string("hello abdul")); }
+	 */
+	 
+	
+	
+	  @Test public void testStudentJson()throws Exception{ 
+		  
+	  mockMvc =  MockMvcBuilders.standaloneSetup(helloResource).build();
+	  mockMvc.perform(get("/student"))
+	  .andExpect(MockMvcResultMatchers.status().isOk())
+	  .andExpect(jsonPath("$.name", Matchers.is("abdul"))); }
+	 
 
 }
